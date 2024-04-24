203001343c463fd5cb3f0a9a20e7a2f99cac1d4f
Kien Bui <kaybi9x@gmail.com>
2024-04-24 18:19:17 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['127.0.0.1', "https://etwadmin.lotusa.net"]
-  }
-}
+    domains: ["127.0.0.1", "https://etwadmin.lotusa.net"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "etwadmin.lotusa.net",
+        port: "",
+        pathname: "/uploads/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
